51bbdb92651f14221716e1b535a837d7e07a8c2d
Chanikya <chanu.reds@gmail.com>
2015-11-15 17:26:24 -0800

    Initial Commit

---
+++ OfferBlast/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OfferBlast
+//
+//  Created by Chanikya on 15/11/2015.
+//  Copyright © 2015 Team Offer Blast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
